cf2da248b0182a9d96051a200412dff32b92a238
John Tuttle <thejwt@gmail.com>
2017-09-11 05:39:52 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
+function addElementToBeginningOfArray(array1,stringElement){
+  return [stringElement, ...array1]
+}
+
+function destructivelyAddElementToBeginningOfArray(stringElement,array1){
+  array1.unshift(stringElement)
+  return [array1]
+}
+
+function addElementToEndOfArray(array1,stringElement){
+  return [...array1, stringElement]
+}
+
+function destructivelyAddElementToEndOfArray(stringElement,array1){
+  array1.push(stringElement)
+  return [array1]
+}
+
+function accessElementInArray(array1,indexNumber){
+  return array1[indexNumber]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array1){
+  array1.shift()
+  return array1
+}
+
+function removeElementFromBeginningOfArray(array1){
+  array1.slice(1,0)
+  return array1
+}
+
+function destructivelyRemoveElementFromEndOfArray (array1){
+  array1.pop()
+  return array1
+}
+
+function removeElementFromEndOfArray (array1){
+  array1.slice(1, array1.length - 1)
+  return array1
+}
